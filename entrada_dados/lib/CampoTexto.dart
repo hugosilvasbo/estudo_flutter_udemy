63710f5dbbadba2fe70186ef6b707a5105df8e8d
Hugo S <hugo_sbo04@hotmail.com>
2022-10-19 08:28:21 -0300

    Entrada de dados

--- entrada_dados/lib/CampoTexto.dart
+++ entrada_dados/lib/CampoTexto.dart
@@ -8,6 +8,9 @@ class CampoTexto extends StatefulWidget {
 }
 
 class _CampoTextoState extends State<CampoTexto> {
+
+  TextEditingController _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +22,34 @@ class _CampoTextoState extends State<CampoTexto> {
             Padding(
               padding: EdgeInsets.all(32),
               child: TextField(
-                keyboardType: TextInputType.text,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Digite um valor"),
+                //enabled: false,
+                //maxLength: 2,
+                //maxLengthEnforcement: true,
+                /*
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.green
+                ),
+                */
+                //obscureText: true,WS
+                /*
+                onChanged: (String e) {
+                  print("Valor digitado: " + e);
+                },
+                */
+                onSubmitted: (String e) {
+                  print("Valor digitado: " + e);
+                },
+                controller: _textEditingController,
               ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print("Valor digitado: " + _textEditingController.text);
+                },
+                child: Text("Salvar")
             )
           ],
         ));
