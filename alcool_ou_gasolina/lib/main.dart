9c075fca2a921ee502383943d2ac92ed62a7be29
Hugo S <hugo_sbo04@hotmail.com>
2022-10-19 08:56:09 -0300

    alcool ou gasolina

---
+++ alcool_ou_gasolina/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:alcool_ou_gasolina/alcoolougasolina.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AlcoolOuGasolina(),
+    )
+  );
+}
