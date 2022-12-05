d373930f4471e300849db546c57032977f89dda6
Hugo S <hugo_sbo04@hotmail.com>
2022-12-04 23:43:14 -0300

    Coisas do flutters

---
+++ youtube/lib/telas/Inicio.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:youtube/Api.dart';
+import 'package:youtube/models/Video.dart';
+
+class Inicio extends StatefulWidget {
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  _listarVIdeos() {
+    Api api = Api();
+    return api.pesquisar("");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Video>>(
+      future: _listarVIdeos(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+            break;
+          case ConnectionState.active:
+          case ConnectionState.done:
+            if (snapshot.hasData) {
+              return ListView.separated(
+                itemBuilder: (context, index) {
+                  List<Video> videos = snapshot.data;
+                  Video video = videos[index];
+
+                  return Column(
+                    children: [
+                      Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(video.imagem),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(video.titulo),
+                        subtitle: Text(video.canal),
+                      ),
+                    ],
+                  );
+                },
+                separatorBuilder: (context, index) => Divider(
+                  height: 2,
+                  color: Colors.grey,
+                ),
+                itemCount: snapshot.data.length,
+              );
+            } else {
+              return Center(
+                child: Text("Nenhum dado a ser exibido"),
+              );
+            }
+            break;
+        }
+      },
+    );
+  }
+}
