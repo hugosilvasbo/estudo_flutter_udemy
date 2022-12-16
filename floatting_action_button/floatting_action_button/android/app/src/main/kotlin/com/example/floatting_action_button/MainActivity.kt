4769b423092928ae137f21fcb96cd5be39c2f0f9
Hugo S <hugo_sbo04@hotmail.com>
2022-12-15 23:06:07 -0300

    FLoatting action button

---
+++ floatting_action_button/floatting_action_button/android/app/src/main/kotlin/com/example/floatting_action_button/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.floatting_action_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ floatting_action_button/floatting_action_button/lib/Home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FloattingActionButton"),
+      ),
+      body: Text("Conteúdo"),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton.extended(
+        //child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+        foregroundColor: Colors.white,
+        elevation: 6,
+        icon: Icon(Icons.add_shopping_cart),
+        label: Text("Adicionar"),
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(8)
+        ),
+        onPressed: () => {},
+        //mini: true,
+        //onPressed: () {
+        //  print("Resultado: botão pressionado!");
+        //}
+      ),
+      bottomNavigationBar: BottomAppBar(
+        //shape: CircularNotchedRectangle(),
+        child: Row(
+          children: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))],
+        ),
+      ),
+    );
+  }
+}
