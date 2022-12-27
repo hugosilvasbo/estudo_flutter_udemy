94f1d4a29850b1fcf83ac973caa46a8617fa4745
Hugo S <hugo_sbo04@hotmail.com>
2022-12-27 08:55:53 -0300

    Banco de dados

--- banco_de_dados/bancodados/lib/main.dart
+++ banco_de_dados/bancodados/lib/main.dart
@@ -39,20 +39,97 @@ class _HomeState extends State<Home> {
         db.execute(sql);
       },
     );
+
+    return retorno;
   }
 
   _salvar() async {
     Database db = await _recuperarBancoDados();
 
-    Map<String, dynamic> dadosUsuario = {"nome": "Hugo Souza", "idade": 25};
+    Map<String, dynamic> dadosUsuario = {
+      "nome": "Maria Aparecida",
+      "idade": 70
+    };
     int id = await db.insert("usuarios", dadosUsuario);
 
     print("Salvo: $id");
   }
 
+  _listarUsuarios() async {
+    Database db = await _recuperarBancoDados();
+
+    String sql = "SELECT * FROM usuarios WHERE nome = 'Maria Aparecida'";
+    List usuarios = await db.rawQuery(sql);
+
+    for (var usuario in usuarios) {
+      print("Item id: " +
+          usuario['id'].toString() +
+          " nome: " +
+          usuario['nome'] +
+          " idade: " +
+          usuario['idade'].toString());
+    }
+
+    //print("usuarios: " + usuarios.toString());
+  }
+
+  _recuperarUsuarioPeloID(int id) async {
+    Database db = await _recuperarBancoDados();
+
+    List usuarios = await db.query(
+      "usuarios",
+      columns: ["id", "nome", "idade"],
+      where: "id = ? AND nome = ? AND idade = ?",
+      whereArgs: [id, "Maria Aparecida", 70],
+    );
+
+    for (var usuario in usuarios) {
+      print("Item id: " +
+          usuario['id'].toString() +
+          " nome: " +
+          usuario['nome'] +
+          " idade: " +
+          usuario['idade'].toString());
+    }
+  }
+
+  _excluirUsuario(int id) async {
+    Database db = await _recuperarBancoDados();
+
+    int retorno = await db.delete(
+      "usuarios",
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    print("item qtde removida: ${retorno}");
+  }
+
+  _atualizarUsuario(int id) async {
+    Database db = await _recuperarBancoDados();
+
+    Map<String, dynamic> dadosUsuario = {
+      "nome": "Maria Aparecida",
+      "idade": 30
+    };
+
+    int retorno = await db.update(
+      "usuarios",
+      dadosUsuario,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    print("Quantidade atualizada: ${retorno}");
+  }
+
   @override
   Widget build(BuildContext context) {
-    _salvar();
+    //_salvar();
+    //_listarUsuarios();
+    //_recuperarUsuarioPeloID(3);
+    //_excluirUsuario(2);
+    _atualizarUsuario(1);
 
     return Container();
   }
